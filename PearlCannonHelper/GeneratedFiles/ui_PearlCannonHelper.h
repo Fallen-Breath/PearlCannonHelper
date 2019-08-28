86e739288c9fead05d7375ae129c6e91d7ead149
Fallen_Breath <yqx791125833@gmail.com>
2019-08-29 00:02:26 +0800

    v2.2

--- PearlCannonHelper/GeneratedFiles/ui_PearlCannonHelper.h
+++ PearlCannonHelper/GeneratedFiles/ui_PearlCannonHelper.h
@@ -694,7 +694,7 @@ public:
         genOutputPushButton->setText(QApplication::translate("PearlCannonHelperClass", "\347\224\237\346\210\220\350\275\250\350\277\271", nullptr));
         tabWidget->setTabText(tabWidget->indexOf(tab_3), QApplication::translate("PearlCannonHelperClass", "\347\217\215\347\217\240\350\275\250\350\277\271\350\276\223\345\207\272", nullptr));
         languagePushButton->setText(QApplication::translate("PearlCannonHelperClass", "\350\257\255\350\250\200\345\210\207\346\215\242", nullptr));
-        CreditLabel->setText(QApplication::translate("PearlCannonHelperClass", "v2.1 by Fallen_Breath", nullptr));
+        CreditLabel->setText(QApplication::translate("PearlCannonHelperClass", "v2.2 by Fallen_Breath", nullptr));
     } // retranslateUi
 
 };
